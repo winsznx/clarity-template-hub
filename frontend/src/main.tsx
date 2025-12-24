e285a7f2b819ec444eaa5dd0b1117c92171f02f1
winsznx <timjosh507@gmail.com>
2025-12-24 13:44:54 +0100

    feat: initialize Reown AppKit at app startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './appkit.config' // Initialize Reown AppKit for WalletConnect support
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
